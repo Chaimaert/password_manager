2b57f54caf268643766c8f385ab68e255a6159dc
chaimaert <chaima.Rouita@emsi-edu.ma>
2024-07-26 11:56:46 +0100

    Update

--- python-passman.py
+++ python-passman.py
@@ -1,6 +1,7 @@
-import hashlib # to hash the password
-import getpass # to hide the password from the user
+import hashlib #securely hash passwords
+import getpass #it securely get user input for passwords without echoing it to the console.
 
+# Dictionary to store username and hashed password
 password_manager = {}
 
 def create_account():
@@ -9,3 +10,32 @@ def create_account():
   hashed_password = hashlib.sha256(password.encode()).hexdigest()
   password_manager[username] = hashed_password
   print("Account created successfully")
+
+
+def login():
+  username = input("Enter your username: ")
+  password = getpass.getpass("Enter your password:")
+  hashed_password = hashlib.sha256(password.encode()).hexdigest()
+  if username in password_manager.keys() and password_manager[username]  == hashed_password : 
+    print("Login successful !")
+  else : 
+    print("Invalid username or password.")
+
+
+def main(): 
+  while True:
+    choice = input("Enter 1 to create an account, 2 to login, or 0 to exit ")
+    if choice == "1":
+      create_account()
+    elif choice == "2":
+      login()
+    elif choice == "0":
+      break
+    else : 
+      print("Invalid choice") 
+  
+
+
+if __name__ == "__main__":
+  main()
+#to import this script in another python script
